cfda78a6ed9781f8079cc1a84a8b4b4a9d18bc93
avieszha <b2029amfpreyes@pshs.edu.ph>
2025-09-16 17:44:44 +0800

    taylor

---
+++ pmo.js
@@ -0,0 +1,8 @@
+let x = 5;
+let square = x * x;
+console.log(square);
+
+for (let i = 0; i < 4; i++) {
+    console.log("You piss me off, you freaking annoying rat.");
+    console.log("I turn around and I see your annoying face talking to annoying rats.");
+}
